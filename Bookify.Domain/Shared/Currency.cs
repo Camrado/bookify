8794f35ab405ecd9932dd1b505e7758143b90dd4
Camrado <blackandwhite.b17w@gmail.com>
2024-06-09 23:00:55 +0400

    Domain Layer Fully Built

---
+++ Bookify.Domain/Shared/Currency.cs
@@ -0,0 +1,23 @@
+﻿namespace Bookify.Domain.Shared;
+
+public record Currency {
+    internal static readonly Currency None = new("");
+    public static readonly Currency EUR = new("EUR");
+    public static readonly Currency USD = new("USD");
+    
+    private string Code { get; init; }
+    
+    private Currency(string code) {
+        Code = code;
+    }
+    
+    public static Currency FromCode(string code) {
+        return All.FirstOrDefault(c => c.Code == code) 
+               ?? throw new ApplicationException($"The currency code '{code}' is invalid.");
+    }
+    
+    public static readonly IReadOnlyCollection<Currency> All = new[] {
+        EUR,
+        USD
+    };
+}
