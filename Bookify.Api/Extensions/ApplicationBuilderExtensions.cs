fa6a6dda72fff4d375940985f85b95d7056f263c
Camrado <blackandwhite.b17w@gmail.com>
2024-06-24 22:16:06 +0400

    Added Structured Logging using Serilog with Seq sink

--- Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
+++ Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -16,4 +16,10 @@ public static class ApplicationBuilderExtensions {
     public static void UseCustomExceptionHandler(this IApplicationBuilder app) {
         app.UseMiddleware<ExceptionHandlingMiddleware>();
     }
+
+    public static IApplicationBuilder UseRequestContextLogging(this IApplicationBuilder app) {
+        app.UseMiddleware<RequestContextLoggingMiddleware>();
+
+        return app;
+    }
 }
--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -1,8 +1,12 @@
 using Bookify.Api.Extensions;
 using Bookify.Application;
 using Bookify.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) => 
+        configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
 
@@ -27,6 +31,10 @@ if (app.Environment.IsDevelopment()) {
 }
 
 app.UseHttpsRedirection();
+
+app.UseRequestContextLogging();
+
+app.UseSerilogRequestLogging();
 
 app.UseCustomExceptionHandler();
 
