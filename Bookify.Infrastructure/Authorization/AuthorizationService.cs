fd89f3edc9c70277081cdafb89fe4da3cac12852
Camrado <blackandwhite.b17w@gmail.com>
2024-06-25 21:31:48 +0400

    Added Distributed Cache via Redis

--- Bookify.Infrastructure/Authorization/AuthorizationService.cs
+++ Bookify.Infrastructure/Authorization/AuthorizationService.cs
@@ -1,16 +1,27 @@
-﻿using Bookify.Domain.Users;
+﻿using Bookify.Application.Abstractions.Caching;
+using Bookify.Domain.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure.Authorization;
 
 internal sealed class AuthorizationService {
     private readonly ApplicationDbContext _dbContext;
+    private readonly ICacheService _cacheService;
     
-    public AuthorizationService(ApplicationDbContext dbContext) {
+    public AuthorizationService(ApplicationDbContext dbContext, ICacheService cacheService) {
         _dbContext = dbContext;
+        _cacheService = cacheService;
     }
 
     public async Task<UserRolesResponse> GetRolesForUserAsync(string identityId) {
+        var cacheKey = $"auth:roles-{identityId}";
+
+        var cacheRoles = await _cacheService.GetAsync<UserRolesResponse>(cacheKey);
+
+        if (cacheRoles is not null) {
+            return cacheRoles;
+        }
+        
         var roles = await _dbContext.Set<User>()
             .Where(user => user.IdentityId == identityId)
             .Select(user => new UserRolesResponse() {
@@ -18,6 +29,8 @@ internal sealed class AuthorizationService {
                 Roles = user.Roles.ToList()
             })
             .FirstAsync();
+
+        await _cacheService.SetAsync(cacheKey, roles);
 
         return roles;
     }
